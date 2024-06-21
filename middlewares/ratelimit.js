a571e7c86a44767541d5182693606000a576ca78
sriramalavalapati3 <sriramalavalapatiit01@gmail.com>
2024-06-21 16:43:54 +0530

    implemented rate limit for login

--- middlewares/ratelimit.js
+++ middlewares/ratelimit.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit');
 const loginLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 5, // Limit each IP to 5 login requests per `window` (here, per minute)
-  message: 'Too many login attempts from this IP, please try again after a minute'
+  message: 'Too many login attempts from this IP, please try again after a minute ' 
 });
 
 
